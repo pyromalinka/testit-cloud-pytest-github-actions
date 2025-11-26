97ac96c6c6457e89f075e7288071d47b4ae63d34
pyromalinka <117767348+pyromalinka@users.noreply.github.com>
2025-11-26 19:20:42 +0300

    Update test_annotations.py

--- tests/test_annotations.py
+++ tests/test_annotations.py
@@ -5,7 +5,7 @@ import testit
 @testit.displayName("with_parameters_success")
 @testit.title("with_parameters_success")
 @pytest.mark.parametrize('number, text', [
-    (1, "string1")
+    (1, "string11111")
 ])
 def test_with_parameters_success(number: int, text: str):
     assert True
